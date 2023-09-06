3b8b599bbb22111c731324ee10f66d47b20826fa
nayfaan <luknnn129@gmail.com>
2023-09-06 05:00:34 -0700

    v.5.0. - Fully functional Flask deployment

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import puni
 from input.__data import items
 
 import itertools
+import re
+from operator import itemgetter
 
 # Flask: Web app wrapper
-from flask import Flask, render_template, request, url_for, redirect, make_response
-from flask import jsonify
+from flask import Flask, render_template, request, url_for, redirect
+from flask import jsonify, make_response
 from markupsafe import escape, Markup
 
 from turbo_flask import Turbo
@@ -52,10 +54,11 @@ def clamp(low, num, high):
     return max(min(high, num), low)
 
 
-def print_result(item_combo, total_score):
-    s = "Score: " + str(total_score) + "\n"
+def print_result(item_combo, total_score, show_icons):
+    s = "<tr><td colspan='100%' style='text-align: left; color: silver;'><b>Score: " + \
+        str(total_score) + "</b></td></tr>"
 
-    for item in item_combo:
+    for item in sorted(item_combo, key=itemgetter(1), reverse=True):
         item_name = item[0][0]
         item_attr = item[0][1]
         item_rank = item[0][2]
@@ -63,18 +66,25 @@ def print_result(item_combo, total_score):
 
         item_add_attr = [i for i in item_attr if i not in item_base_attr]
 
-        print_line = str(item[1]) + " * " + item_name + " (" + item_rank + ")"
+        print_line = "<tr><td>" + str(item[1]) + "</td>" + "<td>*</td><td>"
+
+        if show_icons:
+            print_line += '<img class="item_thumb" src="images/items/' + re.sub("$", ".webp", re.sub(
+                "[\s']", "-", item_name)) + '" onerror="this.onerror=null; this.src=\'images/404.webp\'">'
+
+        print_line += "</td><td>" + item_name + \
+            "</td><td>(" + item_rank + ")</td>"
         if item_add_attr:
-            print_line += (" | " + str(item_add_attr))
-        s += print_line + "\n"
+            print_line += ("<td>Add </td><td>" +
+                           ' '.join(item_add_attr) + "</td>")
+        s += print_line + "</tr>"
 
-    s += "\n"
-    return s
+    s += "<tr><td colspan='100%' style='border-top:2px solid black; height: 1.5em;'></td></tr>"
+    return Markup(s)
 
 
-def zipPerm(puni_target, ordered, permutation, item_category_rank_matrix):
+def zipPerm(puni_target, ordered, show_icons, permutation, item_category_rank_matrix):
     for p in permutation:
-        update_load(p)
         item_permutation = [list(zip(i, p)) for i in itertools.permutations(
             item_category_rank_matrix, len(p))]
 
@@ -87,7 +97,7 @@ def zipPerm(puni_target, ordered, permutation, item_category_rank_matrix):
                 total_score_sort.sort()
 
             if total_score_sort == puni_target:
-                update_load(print_result(item_combo, total_score))
+                update_load(print_result(item_combo, total_score, show_icons))
 
 
 app = Flask(__name__,
@@ -109,29 +119,33 @@ def index():
 
     # where this came from?
     if request.method == "GET":
-        return render_template("index.html", craftable_state=postCheck(True), best_state=postCheck(True), ordered_state=postCheck(False), results="")
+        return render_template("index.html", craftable_state=postCheck(True), best_state=postCheck(True), ordered_state=postCheck(False), icons_state=postCheck(True), results="")
 
     # submit?
     elif request.method == "POST":
+        turbo.push(turbo.replace(
+            Markup("<tbody id='results'></tbody>"), 'results'))
+
         const_ = int(request.form.get('const_'))
         luster = int(request.form.get('luster'))
         mood = int(request.form.get('mood'))
+
         craftable_only = getCheck(request.form.get('craftable_only'))
         best_only = getCheck(request.form.get('best_only'))
         ordered = getCheck(request.form.get('ordered'))
+
+        show_icons = getCheck(request.form.get('show_icons'))
 
         puni_target = [const_, luster, mood]
         settings = (puni_target,
                     (craftable_only, best_only, ordered))
 
         puni_data = puni.run(settings)
-        zipPerm(puni_target, ordered, puni_data[0], puni_data[1])
-
-        # return render_template("index.html", craftable_state = postCheck(craftable_only), best_state = postCheck(best_only), ordered_state = postCheck(ordered), results=results)
+        zipPerm(puni_target, ordered, show_icons, puni_data[0], puni_data[1])
 
     # (?)
     else:
-        return render_template("index.html", craftable_state=postCheck(True), best_state=postCheck(True), ordered_state=postCheck(False), results="")
+        return render_template("index.html", craftable_state=postCheck(True), best_state=postCheck(True), ordered_state=postCheck(False), icons_state=postCheck(True), results="")
 
 
 def main():
---
+++ services/static/script.js
@@ -0,0 +1,66 @@
+function check_color_range(puni_stats) {
+    let fit_color = [];
+    let const_, luster, mood;
+
+    for (color in puni_colors) {
+        const_ = luster = mood = false;
+        if (puni_stats[0] >= puni_colors[color][0] && puni_stats[0] <= puni_colors[color][1]) const_ = true;
+        if (puni_stats[1] >= puni_colors[color][2] && puni_stats[1] <= puni_colors[color][3]) luster = true;
+        if (puni_stats[2] >= puni_colors[color][4] && puni_stats[2] <= puni_colors[color][5]) mood = true;
+
+        if (const_ && luster && mood) fit_color.push(color);
+    }
+
+    return fit_color;
+}
+
+function last_color(fit_color) {
+    let puni_colors = fit_color[0];
+
+    for (const color of fit_color) {
+        const colorIndex = puni_order.indexOf(color);
+
+        if (colorIndex === -1) continue;
+        if (colorIndex < puni_order.indexOf(puni_colors)) puni_colors = color;
+    }
+
+    return puni_colors;
+}
+
+function update_puni_colors() {
+    let const_val = $("#const_").val();
+    let luster_val = $("#luster").val();
+    let mood_val = $("#mood").val();
+
+    let puni_stats = [const_val, luster_val, mood_val];
+
+    let fit_color = check_color_range(puni_stats);
+    let color = last_color(fit_color);
+
+    $("#puni-color").html(color);
+}
+
+const puni_colors = {
+    Shining: [95, 100, 95, 100, 95, 100],
+    Abyss: [0, 1, 0, 1, 0, 1],
+    Big: [80, 100, 30, 100, 30, 100],
+    Flare: [40, 100, 40, 100, 80, 100],
+    Moon: [40, 100, 80, 100, 40, 100],
+    Gold: [20, 100, 75, 100, 20, 100],
+    Silver: [0, 100, 40, 100, 0, 100],
+    Stone: [30, 100, 0, 20, 30, 100],
+    Black: [0, 15, 0, 15, 0, 15],
+    Red: [40, 100, 0, 100, 0, 100],
+    Green: [0, 100, 0, 100, 40, 100],
+    Blue: [0, 100, 0, 100, 0, 100]
+};
+
+const puni_order = ["Shining", "Abyss", "Big", "Flare", "Moon", "Gold", "Silver", "Stone", "Black", "Red", "Green", "Blue"];
+
+$(document).ready(function () {
+    update_puni_colors();
+});
+
+$("#puni-stats-table input").on('input', function () {
+    update_puni_colors();
+});
